49a79cc11ebcd1f5d123fd859ffd9c0df50e98c9
Steve <steve@roaringdevelopment.com>
2018-03-05 06:13:10 -0700

    work with _snapshot data

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+    "mocha": true,
+    "jquery": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "eol-last": ["error", "always"],
+    "no-console": 0,
+    "indent": [
+      "error",
+      "tab"
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+}
