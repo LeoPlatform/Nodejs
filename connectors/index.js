007d073c546df21c56c8c98aaacff0d10dacbc94
Francis Lewis <flewis@leoinsights.com>
2019-05-14 16:25:35 -0600

    DD-1775  * Moving Mongo from leo-sdk into leo-connector-mongodb  * Removing deprecated secretsManager

--- connectors/index.js
+++ connectors/index.js
@@ -8,9 +8,6 @@ module.exports = function (configure) {
 	process.__config.registry = process.__config.registry || {};
 	configure.registry = extend(true, process.__config.registry, configure.registry || {});
 	return {
-		mongo: {
-			oplogTail: require("./mongo/tail.js").stream
-		},
 		elasticsearch: {
 			write: require("./elasticsearch/toES.js")(configure).stream
 		}
