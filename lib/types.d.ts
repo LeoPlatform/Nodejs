35ef5e39b8c31deffb1173adf2fea77c4bc487b9
Clint Zirker <clint.zirker@dsco.io>
2022-03-28 14:08:12 -0600

    Adding missed file types.d.ts

---
+++ lib/types.d.ts
@@ -0,0 +1,96 @@
+/// <reference types="node" />
+import stream from 'stream';
+// export interface LeoStreamOptions extends stream.TransformOptions {
+//     ignoreCommands?: any;
+//     cmd?: any;
+//     cmdFlush?: any;
+//     hasCommands?: any;
+// }
+export interface ReadableStream<T> extends stream.Readable {
+    push(chunk: T, encoding?: BufferEncoding): boolean;
+    read(size?: number): T;
+}
+export interface WritableStream<T> extends stream.Writable {
+    _write(chunk: T, encoding: BufferEncoding, callback?: ErrorCallback): void;
+    write(chunk: T, callback?: (error: Error | null | undefined) => void): boolean;
+    write(chunk: T, encoding: BufferEncoding, callback?: (error: Error | null | undefined) => void): boolean;
+}
+export declare type DuplexStream<T, U> = TransformStream<T, U>;
+export interface TransformStream<T, U> extends stream.Duplex {
+    _write(chunk: T, encoding: BufferEncoding, callback?: ErrorCallback): void;
+    write(chunk: T, callback?: (error: Error | null | undefined) => void): boolean;
+    write(chunk: T, encoding: BufferEncoding, callback?: (error: Error | null | undefined) => void): boolean;
+
+    push(chunk: U, encoding?: BufferEncoding): boolean;
+    read(size?: number): U;
+}
+export declare type DataCallback<T = any, E = Error> = (err?: E | null, data?: T) => void;
+export declare type ErrorCallback = (error: Error | null | undefined) => void;
+export declare type TransformFunction<T = any, E = Error> = (this: stream.Transform, chunk: T, encoding: BufferEncoding, callback: DataCallback<T, E>) => void;
+export declare type RStreamsTransformFunction<T, U, E = Error> = (this: TransformStream<T, U>, obj: T, callback: DataCallback<U, E>) => void;
+export declare type FlushCallback<T = any, E = Error> = (this: stream.Transform, flushCallback: DataCallback<T, E>) => any;
+
+
+export interface Event<T> {
+    id: string;
+    event: string;
+    timestamp: number;
+    event_source_timestamp: number;
+    payload?: T;
+    correlation_id?: {
+        source: string;
+        start?: string;
+        end?: string;
+        units: number;
+    };
+}
+export interface ReadEvent<T> extends Event<T> {
+    eid: string;
+}
+// export interface Event<T> {
+//     id?: string;
+//     timestamp?: number;
+//     event?: string;
+//     eid?: string;
+//     units?: number;
+//     event_source_timestamp: number;
+//     correlation_id: CorrelationId;
+//     payload?: T;
+// }
+export interface CorrelationId {
+    source: string;
+    start: string | number;
+    end?: string | number;
+    units?: number;
+}
+// export interface Buffered<T> {
+//     records: T[];
+//     size: number;
+//     isLast: boolean;
+// }
+// export interface BufferCounter {
+//     size: number;
+//     records: number;
+// }
+// export interface BufferedStream<T> extends TransformStream<T, Buffered<T>> {
+//     reset(): void;
+//     flush(done: DataCallback<null>): void;
+//     updateLimits(limits: any): void;
+//     options?: any;
+// }
+// export interface LeoStats {
+//     source_timestamp?: number | null;
+//     started_timestamp?: number | null;
+//     ended_timestamp?: number | null;
+//     eid?: string | null;
+//     units?: number | null;
+//     start_eid?: string | null;
+// }
+// export interface StatsStream extends TransformStream<any, any> {
+//     checkpoint?: LeoStatsCheckpoint;
+//     get(): LeoStats;
+// }
+// export interface LeoStatsCheckpoint {
+//     (params: any, done: any): any;
+//     stream: TransformStream<any, any>;
+// }
